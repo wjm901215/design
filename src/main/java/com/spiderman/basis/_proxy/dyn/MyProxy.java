382cfeca989b20ef551c64efdd5f3b01c7979de8
wangjm <wangjm@zhiyuaniot.com>
2020-07-01 09:38:47 +0800

    代理模式调正

--- src/main/java/com/spiderman/basis/_proxy/dyn/MyProxy.java
+++ src/main/java/com/spiderman/basis/_proxy/dyn/MyProxy.java
@@ -1,4 +1,4 @@
-package com.spiderman.basis._proxy;
+package com.spiderman.basis._proxy.dyn;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -13,13 +13,9 @@ import java.lang.reflect.Proxy;
  */
 public class MyProxy {
     public static void main(String[] args) {
-        Subject staticProxy = SubjectStaticFactory.getInstance();
-        staticProxy.dealTask("DBQueryTask");
-        staticProxy.dealTask1();
 
         Subject dynameicProxy = DynProxyFactory.getInstance();
         dynameicProxy.dealTask("DBQueryTask");
-        dynameicProxy.dealTask1();
     }
 }
 
@@ -33,11 +29,6 @@ interface Subject {
      * @param taskName 任务名
      */
     void dealTask(String taskName);
-
-    /**
-     * test1
-     */
-    void dealTask1();
 }
 
 /**
@@ -60,61 +51,7 @@ class RealSubject implements Subject {
         }
     }
 
-    /**
-     * test1
-     */
-    @Override
-    public void dealTask1() {
-        System.out.println("dealTask1");
-    }
 }
-
-/**
- * 　代理类，实现了代理接口。
- */
-class ProxySubject implements Subject {
-    //代理类持有一个委托类的对象引用
-    private Subject delegate;
-
-    public ProxySubject(Subject delegate) {
-        this.delegate = delegate;
-    }
-
-    /**
-     * 将请求分派给委托类执行，记录任务执行前后的时间，时间差即为任务的处理时间
-     *
-     * @param taskName
-     */
-    @Override
-    public void dealTask(String taskName) {
-        long stime = System.currentTimeMillis();
-        //将请求分派给委托类处理
-        delegate.dealTask(taskName);
-        long ftime = System.currentTimeMillis();
-        System.out.println("执行任务耗时" + (ftime - stime) + "毫秒");
-
-    }
-
-    /**
-     * test1
-     */
-    @Override
-    public void dealTask1() {
-        delegate.dealTask1();
-    }
-}
-
-/**
- * 生成静态代理类工厂
- */
-class SubjectStaticFactory {
-    //客户类调用此工厂方法获得代理对象。
-    //对客户类来说，其并不知道返回的是代理类对象还是委托类对象。
-    public static Subject getInstance() {
-        return new ProxySubject(new RealSubject());
-    }
-}
-
 
 /**
  * 动态代理类对应的调用处理程序类
